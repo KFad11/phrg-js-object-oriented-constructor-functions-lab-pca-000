407b71f832472299aeccbdc845864083023aeb39
KFad <kfadigan@yahoo.com>
2019-06-19 21:24:28 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+// function User(name, age, hometown) {
+//   this.name = name
+//   this.age = age
+//   this.hometown = hometown
+// }
+
+// let bobby = new User('bobby', 20, 'Philadelphia')
+// // {name: 'bobby', age: 20, hometown: 'Philadelphia'}
+
+function Driver(name, age, experience) {
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
+
+function Scooter(year, color, model) {
+  this.year = year
+  this.color = color
+  this.model = model
+}
+
+function PickupLocation(address, city) {
+  this.address = address
+  this.city = city
+}
